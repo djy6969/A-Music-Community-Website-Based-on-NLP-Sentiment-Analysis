e67d80d664538cf695a4752c851dc5d6d592908c
Karas-lc <82162079+Karas-lc@users.noreply.github.com>
2023-02-21 17:26:38 +0800

    build the front environment and add element-plus & tailwindcss

---
+++ front/src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import ElementPlus from "element-plus";
+import router from "./router";
+
+import "element-plus/dist/index.css";
+import "tailwindcss/tailwind.css";
+import "./assets/main.css";
+
+const app = createApp(App);
+
+app.use(ElementPlus)
+app.use(router);
+
+app.mount("#app");

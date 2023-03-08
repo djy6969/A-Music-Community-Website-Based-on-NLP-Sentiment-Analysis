1cc8fa4dec313b451c7b4b3d82a6bfd73b7ad9d8
liuzi <zijian.liu@ucdconnect.ie>
2023-03-08 16:24:19 +0800

    backend update

--- back-end/app.py
+++ back-end/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True, template_folder='/templete')
 
 # 默认silent = False，即当配置文件不存在时，抛出异常
 app.config.from_pyfile("../common/config/base_setting.py", silent=False)
@@ -11,8 +11,4 @@ app.config.from_pyfile("../common/config/base_setting.py", silent=False)
 # 启动管理
 manager = Manager(app)
 
-# db = SQLAlchemy(app)
-
-# if __name__ == '__main__':
-#     app.run(host='127.0.0.1', port=5000, debug=True)
-
+db = SQLAlchemy(app)
---
+++ back-end/controllers/music.py
@@ -0,0 +1,44 @@
+from app import app, db
+
+from flask import Blueprint, request, render_template
+from common.service import MessageHelper
+import os
+import re
+
+music = Blueprint('music', __name__)
+
+
+# get music id from the front end, and 'id.mp3' is the music name
+# the role of this file is judging whether the file is existed
+@music.route("/getMusicResource", methods=['POST'])
+def getMusicResource():
+    # get music_id from the request
+    music_id = request.json.get('music_id')
+    # music_id = '_uRC-ZabKhY'
+    music_file = music_id + '.mp3'
+    path = os.path.dirname(os.path.abspath(__file__))
+    path = path + '/../static/music/'
+    # relative path of music in project
+    file_name = 'back-end/static/music' + music_id + '.mp3'
+    all_files = os.listdir(path)
+    for i in all_files:
+        if i==music_file:
+            music_filepath = {"music_filepath": file_name}
+            return MessageHelper.ops_renderErrJSON(data=music_filepath)
+    return MessageHelper.ops_renderErrJSON(msg="music doesn't exists.")
+
+
+# get all music resources
+@music.route("/getAllMusicResources", methods=['GET'])
+def getAllMusicResources():
+    path = os.path.dirname(os.path.abspath(__file__))
+    path = path + '/../static/music/'
+    # relative path of music in project
+    print(path)
+    all_files = os.listdir(path)
+    music_list = []
+    for i in all_files:
+        x = re.findall(r'(.*?).mp3', i)
+        music_list.append(x)
+    # return all the music file name
+    return MessageHelper.ops_renderJSON(data=music_list)

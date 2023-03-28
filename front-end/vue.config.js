46f33eb879543a49c1b0d451a3fa08591e68c584
yangdezheng <15801679766@163.com>
2023-03-28 17:06:26 +0800

    update

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   outputDir: "../music",
-  assetsDir: 'static/',
   configureWebpack: {
     devtool: isProd ? false : "source-map",
     devServer: {
